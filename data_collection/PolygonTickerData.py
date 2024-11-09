08a0794df692386518b6838c2b93e3aaa83d16f5
Samuel Kling <samtkling@gmail.com>
2024-11-09 10:34:54 -0800

    add class for polygon 1m data

---
+++ data_collection/PolygonTickerData.py
@@ -0,0 +1,33 @@
+"""
+This class holds 1 minute candle data returned from polygon api
+in the form of:
+    Ticker
+    ResultsCount
+    Results
+    Post High Low
+"""
+class PolygonTickerData:
+    def __init__(self, data):
+        self.ticker = data["ticker"]
+        self.results_count = data["resultsCount"]
+        self.results = data["results"]
+        self.post_high_low = -1
+
+    def print_summary(self):
+        print(self.ticker)
+        print(self.results_count)
+        print(self.results)
+
+    def find_post_high_low(self):
+        if self.post_high_low > 0:
+            return self.post_high_low
+        high_of_day = 0
+        post_high_low = 0
+        for bar in self.results:
+            if bar['h'] > high_of_day:
+                high_of_day = bar['h']
+                post_high_low = bar['c']
+            elif bar['l'] < post_high_low:
+                post_high_low = bar['l']
+        self.post_high_low = post_high_low
+        return self.post_high_low
