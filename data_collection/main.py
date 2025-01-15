b72bb2610205679b338b768454e87097245c2773
Samuel Kling <samtkling@gmail.com>
2025-01-14 20:34:35 -0800

    update main

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,9 +1,8 @@
 from Resource import utils as u
 
 def main():
-    u.run_small_cap_data_collection()     #run small cap data for google sheets
-    # u.run_tradervue_import()              #run the tradervue import
+    u.run_tradervue_import()              # run the tradervue import
+    # u.run_small_cap_data_collection()     # run small cap data for Google sheets
 
 if __name__ == '__main__':
     main()
-
