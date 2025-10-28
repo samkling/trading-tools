3203927b2746de6565a1130c1e0ffde508fce451
Sam Kling <samtkling@gmail.com>
2025-10-28 09:57:37 -0700

    remove hist

--- data_collection/runParams.py
+++ data_collection/runParams.py
@@ -8,14 +8,6 @@ TOS_HISTORY = """
 """
 
 MEDVED_TRADER_HISTORY = """
-Symbol    Order Date    Action    Qty    Sts    Fills    Limit    Order ID    
-CAN    08/14/25 11:18:02 AM    COVER    1,500    Filled     1,500@0.7904        1003947238030    
-CAN    08/14/25 11:17:18 AM    SHORT    1,500    Filled     1,500@0.7953    0.795    1003946387984    
-PSTV    08/14/25 10:48:43 AM    SELL    1,500    Filled     1,500@0.5602 (2)        1003946386431    
-PSTV    08/14/25 10:48:27 AM    BUY    1,500    Filled     1,500@0.5606    0.5606    1003946386408
-Symb    Order Date    Action    Qty    Sts    Fills    Limit    Cmds    O.Type    RmQTY    Order ID    
-IXHL    08/14/25 10:24:33 AM    SELL    1,500    Filled     1,500@0.4129            Market        24    
-IXHL    08/14/25 10:24:05 AM    BUY    1,500    Filled     1,500@0.4131    0.4131        Limit        23    
 """
 
 TICKERS_BY_DAY = [
